3a822e4718fc0d118c3c50243bc39350341d4d50
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-06-09 14:09:33 -0500

    urlDeployServer added

--- src/socket/socket-manager.js
+++ src/socket/socket-manager.js
@@ -2,7 +2,10 @@
 
 import socketIOClient from "socket.io-client";
 
+const urlLocalServer = "http://localhost:8080";
+const urlDeployServer = "https://sphere-websockets-r3f-server.onrender.com";
+
 /**
  * Socket connection
  */
-export const socket = socketIOClient("http://localhost:8080");
+export const socket = socketIOClient(urlDeployServer);
